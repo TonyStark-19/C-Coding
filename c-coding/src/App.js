bc0d1e3e734a5c2c421c01e1c56874be8bfcea91
Aditya chandel <adityachandel371@gmail.com>
2025-05-05 18:00:36 +0530

    Installed react dependencies

---
+++ c-coding/src/App.js
@@ -0,0 +1,16 @@
+// css
+import './App.css';
+
+export default function MainPage() {
+    return (
+        <h1>hii</h1>
+    )
+}
+
+function Navbar() {
+    return (
+        <div className='navbar'>
+            
+        </div>
+    )
+}
