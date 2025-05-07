1fb7633569d9293b41d829ecfa400eab90273074
Aditya chandel <adityachandel371@gmail.com>
2025-05-07 20:19:16 +0530

    Added content

--- c-coding/src/Levels.jsx
+++ c-coding/src/Levels.jsx
@@ -1,12 +1,16 @@
+// css
+import './levels.css';
+
+// import navbar
 import { Navbar } from './Mainpage';
 
 // main page
 export default function LevelPage() {
     return (
-        <>
+        <div className='container'>
             <Navbar />
             <Levels />
-        </>
+        </div>
     )
 }
 
@@ -19,14 +23,17 @@ function Levels() {
                 <div className="level-card beginner">
                     <h2>Beginner</h2>
                     <p>Simple logic, basic syntax, loops, and conditionals.</p>
+                    <p>Number of Programs : 22</p>
                 </div>
                 <div className="level-card intermediate">
                     <h2>Intermediate</h2>
                     <p>Functions, arrays, strings, and slightly complex logic.</p>
+                    <p>Number of Programs : 39</p>
                 </div>
                 <div className="level-card advanced">
                     <h2>Advanced</h2>
                     <p>Pointers, dynamic memory, data structures, and logic puzzles.</p>
+                    <p>Number of Programs : 16</p>
                 </div>
             </div>
         </div>
