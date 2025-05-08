0b1bf50f31fa319bbe01edefe56b8c8d56ece9da
Aditya chandel <adityachandel371@gmail.com>
2025-05-08 22:36:01 +0530

    Edited router

--- c-coding/src/App.js
+++ c-coding/src/App.js
@@ -1,17 +1,22 @@
 // router
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-// link pages
+// pages
 import MainPage from './Mainpage';
-import Levels from './Levels';
+import LevelPage from './Levels';
+import EasyPage from './Levels/EasyPage';
+import MediumPage from './Levels/MediumPage';
+import HardPage from './Levels/HardPage';
 
-// app
 export default function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<MainPage />} />
-                <Route path="/levels" element={<Levels />} />
+                <Route path="/levels" element={<LevelPage />} />
+                <Route path="/levels/easy" element={<EasyPage />} />
+                <Route path="/levels/medium" element={<MediumPage />} />
+                <Route path="/levels/hard" element={<HardPage />} />
             </Routes>
         </Router>
     );
