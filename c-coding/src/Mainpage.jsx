7ea5a145e378e17d5a89074acbaa59f02cd8e907
Aditya chandel <adityachandel371@gmail.com>
2025-05-12 21:55:24 +0530

    Created footer

--- c-coding/src/Mainpage.jsx
+++ c-coding/src/Mainpage.jsx
@@ -28,6 +28,7 @@ export default function MainPage() {
         <div className='container'>
             <Navbar />
             <Home />
+            <Footer />
         </div>
     )
 }
@@ -62,6 +63,41 @@ function Home() {
                 <Link to="/levels">
                     <button className='get-started-button' data-aos="zoom-in" data-aos-delay="400" data-aos-duration="800">Get Started</button>
                 </Link>
+            </div>
+        </div>
+    )
+}
+
+// footer
+export function Footer() {
+    return (
+        <div className='footer'>
+            <div className='footer-content'>
+                <div className="footer-section">
+                    <h3>About</h3>
+                    <p>This site was built to help learners practice and master C programming by solving categorized problems.</p>
+                </div>
+
+                <div className="footer-section">
+                    <h3>Quick Links</h3>
+                    <ul>
+                        <li><Link to="/levels/easy">Easy</Link></li>
+                        <li><Link to="/levels/medium">Medium</Link></li>
+                        <li><Link to="/levels/hard">Hard</Link></li>
+                    </ul>
+                </div>
+
+                <div className='footer-section'>
+                    <h3>More links</h3>
+                    <ul>
+                        <li><a href="https://github.com/TonyStark-19/C-Coding" target="_blank" rel="noopener noreferrer">GitHub Repo</a></li>
+                        <li><a href='https://youtu.be/irqbmMNs2Bo?si=J765kMmaJwVFryig'>Apna college one shot C tutorial</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className="footer-bottom">
+                <p>Made with ❤️ by Aditya chandel</p>
             </div>
         </div>
     )
